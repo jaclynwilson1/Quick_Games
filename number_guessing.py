d3b2aca3eed76a02d4860320c7872f6f78a0405d
jaclynwilson1 <jaclyn@Jaclyns-MacBook-Pro.local>
2023-10-31 12:54:36 -0400

    total guesses

--- number_guessing.py
+++ number_guessing.py
@@ -4,6 +4,7 @@ import time
 posible_nums = list(range(1, 101))
 continue_playing = True
 guessed_numbers = []
+total_guesses = 0
 
 print("Pick a number between 1 and 100.")
 start = input("Do you have one?")
@@ -14,10 +15,12 @@ while continue_playing == True:
         continue
     else:
         print("I think " + str(guess) + " is your number.")
+        total_guesses += 1
         time.sleep(1)
         ans = input("Is it your number? [y/n]")
         if ans == "y":
             print("Yay!")
+            print("I guessed your number in " + str(total_guesses) +" guesses!")
             continue_playing = False
         else:
             guessed_numbers.append(guess)
